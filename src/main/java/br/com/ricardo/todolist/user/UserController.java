504e3558a3cbf1063dd1312bdb8bb8a318b91d46
Ricardo <rockobaschirotto@gmail.com>
2023-10-11 13:44:18 -0300

    Tasks

--- src/main/java/br/com/ricardo/todolist/user/UserController.java
+++ src/main/java/br/com/ricardo/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -24,8 +26,12 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já cadastrado.");
         }
 
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+
         var userCreated = this.userRepository.save(userModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Cadastrado com sucesso.");
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
 }
